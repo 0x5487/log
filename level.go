5987be3a3a656cc8a0e17308fdd0817d625b90e2
Jason Lee <jawc@hotmail.com>
2016-09-09 19:20:22 +0800

    add minlevel feature

--- level.go
+++ level.go
@@ -1,5 +1,7 @@
 package log
 
+import "strings"
+
 // Level of the log
 type Level uint8
 
@@ -35,4 +37,44 @@ var levelNames = []string{
 // String returns the string representation of a logging level.
 func (p Level) String() string {
 	return levelNames[p]
+}
+
+func GetLevelsFromMinLevel(minLevel string) []Level {
+	minLevel = strings.ToLower(minLevel)
+	switch minLevel {
+	case "debug":
+		return AllLevels
+	case "info":
+		return []Level{
+			InfoLevel,
+			WarnLevel,
+			ErrorLevel,
+			PanicLevel,
+			FatalLevel,
+		}
+	case "warn":
+		return []Level{
+			WarnLevel,
+			ErrorLevel,
+			PanicLevel,
+			FatalLevel,
+		}
+	case "error":
+		return []Level{
+			ErrorLevel,
+			PanicLevel,
+			FatalLevel,
+		}
+	case "panic":
+		return []Level{
+			PanicLevel,
+			FatalLevel,
+		}
+	case "fatal":
+		return []Level{
+			FatalLevel,
+		}
+	default:
+		return AllLevels
+	}
 }
