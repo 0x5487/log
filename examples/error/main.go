8f91e50faf3b5483bb6c5ad0091e06eaf86fc48d
JasonLee <jawc@hotmail.com>
2020-01-29 13:07:32 +0800

    remove pkg/error dependency

--- examples/error/main.go
+++ examples/error/main.go
@@ -1,58 +1,58 @@
-package main
+// package main
 
-import (
-	"errors"
-	"fmt"
+// import (
+// 	"errors"
+// 	"fmt"
 
-	"github.com/jasonsoft/log"
-	"github.com/jasonsoft/log/handlers/console"
-	pkgErr "github.com/pkg/errors"
-)
+// 	"github.com/jasonsoft/log"
+// 	"github.com/jasonsoft/log/handlers/console"
+// 	pkgErr "github.com/pkg/errors"
+// )
 
-var ErrNotFound = pkgErr.New("record not found")
+// var ErrNotFound = pkgErr.New("record not found")
 
-func main() {
-	clog := console.New()
-	log.RegisterHandler(clog, log.AllLevels...) // use console handler to log all level log
+// func main() {
+// 	clog := console.New()
+// 	log.RegisterHandler(clog, log.AllLevels...) // use console handler to log all level log
 
-	err := http()
-	if err != nil {
-		if errors.Is(err, ErrNotFound) {
-			fmt.Printf("err is ErrNotFound: %v\n", err)
-		}
+// 	err := http()
+// 	if err != nil {
+// 		if errors.Is(err, ErrNotFound) {
+// 			fmt.Printf("err is ErrNotFound: %v\n", err)
+// 		}
 
-		var errDup error
-		if errors.As(err, &errDup) {
-			fmt.Printf("err as : %v\n", errDup)
-		}
+// 		var errDup error
+// 		if errors.As(err, &errDup) {
+// 			fmt.Printf("err as : %v\n", errDup)
+// 		}
 
-		if pkgErr.Cause(err) == ErrNotFound {
-			fmt.Printf("root ErrNotFound error: %v\n", err)
-		}
+// 		if pkgErr.Cause(err) == ErrNotFound {
+// 			fmt.Printf("root ErrNotFound error: %v\n", err)
+// 		}
 
-		// format the error to a string and print it
-		//formattedStr := eris.ToString(err, true)
+// 		// format the error to a string and print it
+// 		//formattedStr := eris.ToString(err, true)
 
-		log.WithError(err).Error("oops")
-	}
-}
+// 		log.WithError(err).Error("oops")
+// 	}
+// }
 
-func repo() error {
-	return pkgErr.Wrap(ErrNotFound, "id 6 was not found")
-}
+// func repo() error {
+// 	return pkgErr.Wrap(ErrNotFound, "id 6 was not found")
+// }
 
-func service() error {
-	err := repo()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func service() error {
+// 	err := repo()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func http() error {
-	err := service()
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func http() error {
+// 	err := service()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
--- go.mod
+++ go.mod
@@ -5,6 +5,5 @@ go 1.13
 require (
 	github.com/fatih/color v1.9.0
 	github.com/mattn/go-colorable v0.1.4
-	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.3.0
 )
